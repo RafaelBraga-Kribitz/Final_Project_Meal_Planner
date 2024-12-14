6e857f27ff31b40e9c3ac665b8b1de81da99e1f1
Marina Stankovic <marina.stank990@gmail.com>
2024-12-14 19:15:29 +0100

    Fix meal_planner logic (for approved recipes), Add link to recipes in meal_pleanner show page

--- group6_final_project-master/src/Form/MealPlannerType.php
+++ group6_final_project-master/src/Form/MealPlannerType.php
@@ -4,9 +4,9 @@ namespace App\Form;
 
 use App\Entity\MealPlanner;
 use App\Entity\Recipe;
-use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,26 +14,24 @@ class MealPlannerType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $approvedRecipes = $options['approved_recipes'];
         $builder
-            ->add('chosen_date', null, [
-                'widget' => 'single_text',
-            ])
-          
+            ->add('chosen_date', DateType::class)
             ->add('meal_chosen', EntityType::class, [
                 'class' => Recipe::class,
-                'choice_label' => 'name', //display name instead of id 
+                'choices' => $approvedRecipes,
                 'multiple' => true,
-                'required' => false,
-            ])
-          
-           
-        ;
+                'expanded' => false,
+                'choice_label' => 'name',
+                'choice_value' => 'id',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => MealPlanner::class,
+            'approved_recipes' => []
         ]);
     }
 }
