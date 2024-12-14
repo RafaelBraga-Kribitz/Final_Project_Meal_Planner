ab5de2db41fcba196d5a83ecccadb46de9381e30
Marina Stankovic <marina.stank990@gmail.com>
2024-12-14 17:27:25 +0100

    add code for RecipeController.php

--- group6_final_project-master/src/Controller/RecipeController.php
+++ group6_final_project-master/src/Controller/RecipeController.php
@@ -2,20 +2,102 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Recipe;
+use App\Form\RecipeType;
 use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
-class RecipeController extends AbstractController
+#[Route('/home/admin/recipe', name: 'admin_')]
+final class RecipeController extends AbstractController
 {
+    #[Route('/',name: 'app_recipe_index', methods: ['GET'])]
     public function index(RecipeRepository $recipeRepository): Response
     {
-        // Fetch all recipes from the database
-        $recipes = $recipeRepository->findAll();
-
-        // Pass the recipes to the Twig template
         return $this->render('recipe/index.html.twig', [
-            'recipes' => $recipes,
+            'recipes' => $recipeRepository->findAll(),
         ]);
+    }
+
+    #[Route('/pending', name: 'app_pending', methods: ['GET'])]
+    public function pending(RecipeRepository $recipeRepository): Response
+    {
+        return $this->render('recipe/pending.html.twig', [
+            'recipes' => $recipeRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/approve/{id}', name: 'app_approve', methods: ['GET', 'POST'])]
+    public function approve(Recipe $recipe, EntityManagerInterface $em): Response
+    {
+        $recipe->setStatus(1);
+        $em->persist($recipe);
+        $em->flush();
+        return $this->redirectToRoute('admin_app_recipe_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/new', name: 'app_recipe_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $recipe = new Recipe();
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+        $user = $this->getUser();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $recipe->setAuthor($user);
+            $recipe->setStatus(True);
+            $entityManager->persist($recipe);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_app_recipe_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('recipe/new.html.twig', [
+            'recipe' => $recipe,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_recipe_show', methods: ['GET'])]
+    public function show(Recipe $recipe): Response
+    {
+        return $this->render('recipe/show.html.twig', [
+            'recipe' => $recipe,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_recipe_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Recipe $recipe, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+        $user = $this->getUser();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $recipe->setStatus(True);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_app_recipe_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('recipe/edit.html.twig', [
+            'recipe' => $recipe,
+            'form' => $form,
+            'userId' => $user->getId(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_recipe_delete', methods: ['POST'])]
+    public function delete(Request $request, Recipe $recipe, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$recipe->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($recipe);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_app_recipe_index', [], Response::HTTP_SEE_OTHER);
     }
 }
