3d2950a81025c2f04df9d62b8da7c3bb25e9e5ef
Yosyp Malanka <malankajosyp@gmail.com>
2024-12-15 11:36:20 +0100

    adding alert by creating a new recipe

--- group6_final_project-master/public/js/app.js
+++ group6_final_project-master/public/js/app.js
@@ -1,11 +1,13 @@
-
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbo:load', () => {
+  
   const alerts = document.querySelectorAll('.alert');
+
   alerts.forEach(alert => {
       setTimeout(() => {
           alert.classList.add('fade-out');
-          setTimeout(() => alert.remove(), 500); 
+          setTimeout(() => {
+              alert.remove();
+          }, 500); 
       }, 5000); 
   });
 });
